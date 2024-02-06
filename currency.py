3abeaa49948bb52b8d299831a0edeb8dd2926264
Aashish Thapa <aashishthapa@Aashishs-MacBook-Pro.local>
2024-02-06 10:10:32 +0545

    test environment

--- currency.py
+++
@@ -1,9 +0,0 @@
-def special_format(n):
-    n = n.replace(',', '')
-    s, *d = str(n).partition(".")
-    r = ",".join([s[x-2:x] for x in range(-3, -len(s), -2)][::-1] + [s[-3:]])
-    return "".join([r] + d)
-
-
-number = "1,000,000.0080000"
-print(special_format(number))
